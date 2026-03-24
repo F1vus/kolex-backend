076da29c02310d4ff6a519e0ffc2aa594eaeaf02
Fiv_ <nzaikar184@gmail.com>
2026-03-24 12:22:28 +0100

    init

---
+++ src/main/java/net/f1v/kolexbackend/KolexBackendApplication.java
@@ -0,0 +1,13 @@
+package net.f1v.kolexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolexBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/f1v/kolexbackend/KolexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.f1v.kolexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
