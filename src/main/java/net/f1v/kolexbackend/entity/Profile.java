b4b150ad41a2699b582f1ec93765f2a13e65aa8b
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2026-04-11 11:52:58 +0200

    add user and profile entities

---
+++ src/main/java/net/f1v/kolexbackend/entity/Profile.java
@@ -0,0 +1,42 @@
+package net.f1v.kolexbackend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "profiles", schema = "backend")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id")
+    private Long id;
+
+    @Column(name = "profile_first_name", nullable = false, length = 255)
+    private String firstName;
+
+    @Column(name = "profile_last_name", nullable = false, length = 255)
+    private String lastName;
+
+    @Column(name = "profile_created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
