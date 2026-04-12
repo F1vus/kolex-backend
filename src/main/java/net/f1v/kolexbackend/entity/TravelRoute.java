e848393139766b381f53fff630985a8272854053
RealMan <maxim27112711@gmail.com>
2026-04-12 19:24:36 +0200

    I created and added entities for project "KOLEX"

---
+++ src/main/java/net/f1v/kolexbackend/entity/TravelRoute.java
@@ -0,0 +1,47 @@
+package net.f1v.kolexbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+
+@Entity
+@Table(name = "travel_route", schema = "backend",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "un_travel_station",
+                        columnNames = {"travel_id", "travel_station_stop_id"})
+        })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TravelRoute {
+
+    @EmbeddedId
+    private TravelRouteId id;
+
+    @ManyToOne
+    @MapsId("travelId")
+    @JoinColumn(name = "travel_id", nullable = false)
+    private Travel travel;
+
+    @ManyToOne
+    @JoinColumn(name = "travel_station_stop_id", nullable = false)
+    private Station station;
+
+    @Column(name = "travel_distance", nullable = false)
+    private Integer distance;
+
+    @Column(name = "travel_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "arrival_offset")
+    private Duration arrivalOffset;
+
+    @Column(name = "departure_offset")
+    private Duration departureOffset;
+}
